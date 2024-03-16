af4fdad53e87592e24c13d2541c9391dc95cb561
PRACHET PATEL <prachet35@gmail.com>
2024-03-16 17:19:38 +0530

    feat:developed dynamiac webpage for projects

--- router/apiRouter.js
+++ router/apiRouter.js
@@ -3,8 +3,8 @@ const { contactForm } = require('../controller/contact');
 const router=express.Router();
 const path=require('path');
 const fs=require('fs');
-const profile = require('../models/profile.model');
-
+// const project = require('../models/project.model');
+// const profile = require('../models/profile.model');
 router.post("/contact",contactForm);
 router.get('/resume/:id',(req,res)=>{
     const filePath = path.join(__dirname, '../securefiles', 'resume.pdf'); // Path to your secure PDF file
@@ -19,24 +19,44 @@ router.get('/resume/:id',(req,res)=>{
         readStream.pipe(res);
     }
 });
-router.post("/profile",async (req,res)=>{
-    const {name,email,homepage_heading,homepage_description,aboutpage_description,about,skills,projectspage_description,footer_description,links,developer}=req.body;
-    // console.log(name,email,message);
-    const profileRecord=await profile.create({
-        name:name,
-        email:email,
-        homepage_heading:homepage_heading,
-        homepage_description:homepage_description,
-        aboutpage_description:aboutpage_description,
-        about:about,
-        skills:skills,
-        projectspage_description:projectspage_description,
-        footer_description:footer_description,
-        links:links,
-        developer:developer
-    })
-    if(profileRecord){
-        res.json({status:200,message:"Response Recoded!!,",profile:JSON.stringify(profileRecord)});
-    }
-});
+// router.post("/profile",async (req,res)=>{
+//     const {name,email,homepage_heading,homepage_description,aboutpage_description,about,skills,projectspage_description,footer_description,links,developer}=req.body;
+//     // console.log(name,email,message);
+//     const profileRecord=await profile.create({
+//         name:name,
+//         email:email,
+//         homepage_heading:homepage_heading,
+//         homepage_description:homepage_description,
+//         aboutpage_description:aboutpage_description,
+//         about:about,
+//         skills:skills,
+//         projectspage_description:projectspage_description,
+//         footer_description:footer_description,
+//         links:links,
+//         developer:developer
+//     })
+//     if(profileRecord){
+//         res.json({status:200,message:"Response Recoded!!,",profile:JSON.stringify(profileRecord)});
+//     }
+// });
+// router.post("/projects",async (req,res)=>{
+//     const {project_name,project_type,project_description,projectspage_heading,projectspage_description,projectspage_overview,projectspage_features,techstack,projectsource_link,project_link,developers}=req.body;
+//     // console.log(name,email,message);
+//     const projectRecord=await project.create({
+//         project_name:project_name,
+//         project_type:project_type,
+//         project_description:project_description,
+//         projectspage_heading:projectspage_heading,
+//         projectspage_description:projectspage_description,
+//         projectspage_overview:projectspage_overview,
+//         projectspage_features:projectspage_features,
+//         techstack:techstack,
+//         projectsource_link:projectsource_link,
+//         project_link:project_link,
+//         developers:developers
+//     })
+//     if(projectRecord){
+//         res.json({status:200,message:"Response Recoded!!,",project:JSON.stringify(projectRecord)});
+//     }
+// });
 module.exports=router;
--- router/staticRouter.js
+++ router/staticRouter.js
@@ -3,11 +3,25 @@ const router=express.Router();
 const path=require('path');
 const fs=require('fs');
 const profile = require('../models/profile.model');
+const objectIdRegex = /^[0-9a-fA-F]{24}$/;
+const project = require('../models/project.model'); 
 router.get('/',async (req,res)=>{
     const profiledata=await profile.findOne();
-    res.render('index', {profiledata:profiledata});
+    const projectsdetails=await project.find();
+    res.render('index', {profiledata:profiledata,projectsdetails:projectsdetails});
 })
-router.get('/projectdetails',(req,res)=>{
-
+router.get('/projectdetails/:id',async (req,res)=>{
+    if(objectIdRegex.test(req.params.id)){
+        res.status(200);
+        const profiledata=await profile.findOne();
+        const projectsdetails=await project.findById(req.params.id);
+        if(projectsdetails){
+            res.render('projects', {profiledata:profiledata,projectsdetails:projectsdetails});
+        }else{
+            res.redirect("/");
+        }
+    }else{
+        res.redirect("/");
+    }
 })
 module.exports=router;
